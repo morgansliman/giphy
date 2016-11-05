9154d4c9118c3cff7d1f674bfcc550a62f890725
morgansliman <morgansliman@gmail.com>
2016-11-04 20:51:10 -0400

    displays gifs on button press and gifs start/stop on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,9 +23,9 @@ $(document).ready(function() {
 		'frog'
 	];
 
-	var baseURL = 'http://api.giphy.com/v1/gifs/search?q=';
-	var key = '&api_key=dc6zaTOxFJmzC';
-	var limitURL = '&limit=10';
+	var baseURL = 'http://api.giphy.com/v1/gifs/search?q=',
+	 	key = '&api_key=dc6zaTOxFJmzC',
+	 	limitURL = '&limit=10';
 
 	for (var i = 0; i < topics.length; i++) {
 		$('.button-list').append(
@@ -34,25 +34,53 @@ $(document).ready(function() {
 		}).text(topics[i]));
 	}
 
+
 	$('.button-topic').on('click', function() {
 		console.log(baseURL + encodeURIComponent($(this).text()) + key + limitURL);
+
+		source = {};
+
 		$.ajax({
 			url: (baseURL + encodeURIComponent($(this).text()) + key + limitURL),
 			method: 'GET'
 		}).done(function(response) {
+
 			console.log(response);
+			
 			$('.gif-list').empty();
-			for (var i = 0; i < response.data.length; i++) {
+
+			var i = 0;
+			for (var j = 0; j < 4; j++) {
 				$('.gif-list').append(
 					$('<div>', {
-						'class': 'gif-wrapper'
-					}).html(
-						'<p class="gif-rating">Rating: ' + response.data[i].rating + '</p>' +
-						'<br>' +
-						'<img class="gif" src="' + response.data[i].images.fixed_width_still.url + '">'
+						'class': 'row'
+					})
+				)
+
+				do {
+					$('.gif-list').children().eq(-1).append(
+						$('<div>', {
+							'class': 'gif-wrapper'
+						}).html(
+							'<p class="gif-rating">Rating: ' + response.data[i].rating + '</p>' +
+							'<br>' +
+							'<img class="gif" src="' + response.data[i].images.fixed_width_still.url + '"' +
+							'data-id="' + response.data[i].id + '">'
+						)
 					)
-				);
-			}
+
+					source[response.data[i].id] = response.data[i].images.fixed_width.url;
+					i += 1;
+				} while ( (i % 3) != 0 && i < 10);
+			} 
+
+			$('.gif').on('click', function() {
+				var temp = $(this).attr('src');
+				$(this).attr('src', source[$(this).data('id')]);
+				source[$(this).data('id')] = temp;
+			});
 		});
 	});
+
+
 });
